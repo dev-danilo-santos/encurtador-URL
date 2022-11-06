c40369b7a3009d9f51afb9ecf98a8dd8aed94dc1
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-11-05 22:47:00 -0300

    Realizada a clonagem do projeto. Iniciando resolução do mesmo

---
+++ src/main/java/br/com/gomide/data_structures/hash_map/URLShortener.java
@@ -0,0 +1,23 @@
+package br.com.gomide.data_structures.hash_map;
+
+public class URLShortener implements IURLShortener {
+
+	@Override
+	public String encode(String url) throws RuntimeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String decode(String encodedUrl) throws RuntimeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPrefix() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
